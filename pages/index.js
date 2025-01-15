4681472f82b063b47e208733adce23e396561bd6
Aju Chacko <ajuchacko91@gmail.com>
2025-01-15 13:27:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Lessons({ sections }) {
         <meta name="og:title" content={courseInfo.title}></meta>
         <meta name="og:image" content={`/images/social-share-cover.jpg`}></meta>
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <base target="_blank"></base>
       </Head>
       <div>
         <div className="jumbotron">
